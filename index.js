0373186e2401add61afb9badac3ed017f1f009e4
mohd-irsad <mdirsad9616@gmail.com>
2024-04-01 00:24:35 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+gsap.from(".ani",{
+    opacity:0,
+    x:50,
+    duration:3,
+    delay:0.5,
+    stagger:0.2
+})
+
+
+gsap.to(".btn",{
+    opacity:1,
+    x:50,
+    duration:3,
+    delay:3,
+})
